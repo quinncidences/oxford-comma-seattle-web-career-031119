c5cf077de4cf89ce8bf0a332a902ad41b1f21b13
Quinn Cox <qrc.inpa@gmail.com>
2019-03-16 14:40:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
-  if array.length == 2
+  elsif array.length == 2
     return array.join(" and ")
-  if array.length >= 3
+  elsif array.length >= 3
     array[-1] = array.join("and ")
     return array.join(", ")
   end
