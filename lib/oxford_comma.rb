2702d1eabe005e4ff19b21c4c3e4cc9934f10d86
Quinn Cox <qrc.inpa@gmail.com>
2019-03-16 14:37:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  if array.length = 1
-return array.join(" and ")
+  if array.length == 1
+    array.join
+  if array.length == 2
+    array.join(" and ")
+  if array.length == 3
+    
 end
